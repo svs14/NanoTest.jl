cdbdbbb8843cf5afc28922ffb7725d373051be09
Samuel Jenkins <jenkins.j.samuel@gmail.com>
2014-08-03 22:14:23 +1000

    Barebones example of what to be built for BDD.

--- src/NanoTest.jl
+++ src/NanoTest.jl
@@ -1,5 +1,21 @@
 module NanoTest
 
-# package code goes here
+export describe,
+       before,
+       after,
+       it,
+       @expect
+
+function before(func::Function)
+end
+function after(func::Function)
+end
+function describe(func::Function, desc::String)
+end
+function it(func::Function, desc::String)
+end
+
+macro expect(ex)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,30 @@
 using NanoTest
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Example spec runs without error (derived from Ruby MiniTest)
+type Meme end
+i_can_haz_cheezburger(_::Meme) = "OHAI!"
+will_it_blend(_::Meme) = "YES!"
+
+describe("Meme") do
+  local meme::Meme
+
+  before() do
+    meme = Meme()
+  end
+  after() do
+    nothing
+  end
+
+  describe("when asked about cheeseburgers") do
+    it("must respond positively") do
+      @expect i_can_haz_cheezburger(meme) == "OHAI!"
+    end
+  end
+
+  describe("when asked about blending possibilites") do
+    it("won't say no") do
+      @expect will_it_blend(meme) != "NO!"
+    end
+  end
+end
